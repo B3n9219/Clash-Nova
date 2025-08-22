e32bc43e0ee58a94b93210a52b171109d1b86e93
B3n9219 <benkirk1441@gmail.com>
2025-08-22 12:17:01 +0100

    Clan page fetches data from api using api util

--- src/pages/Clan.jsx
+++ src/pages/Clan.jsx
@@ -1,10 +1,36 @@
 import { useParams } from "react-router-dom";
+import {useEffect, useState} from "react";
+
+import { getClanSummaryInfo } from "../api/main.js";
 
 function Clan() {
     const { tag } = useParams();
+    const [clanData, setClanData] = useState({
+        summary: null,
+    })
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
+    useEffect(() => {
+        async function fetchClanData() {
+            try {
+                const summaryData = await getClanSummaryInfo(tag)
+                setClanData({...clanData, summary: summaryData})
+                setError(null)
+            } catch (error) {
+                setError(error.message)
+                setClanData(null)
+            } finally {
+                setLoading(false)
+            }
+        }
+        fetchClanData()
+    }, [])
+    if (loading) return <p>Loading...</p>
+    if (error) return <p>{error}</p>
     return (
         <>
             <p>Clan tag: {tag}</p>
+            <p>{clanData}</p>
         </>
     )
 }
